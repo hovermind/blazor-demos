4846d044c517bc00faebe3ee3b0fcf9595fa9f0c
HASSAN ハッサン <console041@gmail.com>
2020-08-25 21:58:59 +0900

    Add Azure App Service Easy Auth project

---
+++ AzureAppServiceEasyAuth/Extensions/AzureAppServiceEasyAuthMiddlewareExtension.cs
@@ -0,0 +1,13 @@
+﻿using AzureAppServiceEasyAuth.Middlewares;
+using Microsoft.AspNetCore.Builder;
+
+namespace AzureAppServiceEasyAuth.Extensions
+{
+    public static class AzureAppServiceEasyAuthMiddlewareExtension
+    {
+        public static IApplicationBuilder UseAppServiceEasyAuth(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<AzureAppServiceEasyAuthMiddleware>();
+        }
+    }
+}
